e709427e31d40bf8faed449f13759c22bddee368
tdelpier <tdelpier@gmail.com>
2025-01-29 13:03:22 -0500

    added light colors to default values

--- R/fa_setup_default_values.R
+++ R/fa_setup_default_values.R
@@ -9,10 +9,17 @@
 fa_setup_default_values <- function() {
   
   mea_red <<- "#C2002F"
+  mea_red_light <<- "#fad1db"
+  
   mea_green <<- "#00A651"
+  mea_green_light <<- "#C2FFDF"
+  
   mea_blue <<- "#003057"
+  mea_blue_light <<- "#d1e8fa"
+  
   mea_gray <<- "#3F3F3F"
-  mea_lightblue <<- "#009CDE"
+  mea_gray_light <<- "#e3e3e3"
+
   line_size <<- 1.25
   text_size <<- 10
   
