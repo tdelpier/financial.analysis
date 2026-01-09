bc415777b6e0213f88b9ba5883c2e836163f2924
tdelpier <tdelpier@gmail.com>
2026-01-09 12:46:17 -0500

    fixing geo data imput error

--- R/fa_import_geo.R
+++ R/fa_import_geo.R
@@ -13,7 +13,8 @@ fa_import_geo <- function() {
   tt_import_geo_district() %>% 
     sf::st_set_crs(4269) %>% 
     sf::st_transform(4269) %>%   
-    smoothr::drop_crumbs(threshold = units::set_units(1, km^2)) %>% 
+    # smoothr::drop_crumbs(threshold = units::set_units(1, km^2)) %>%
+      # dropping crumbs was causing an error that I couldn't figure out
     mutate(c_id = row_number()) %>%   
     # st_crop(xmin = -82.4, xmax = -90.35, ymin = 41.7, ymax = 47.5) %>%
     sf::st_make_valid()
