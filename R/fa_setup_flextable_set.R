51d7f5a549a5cc41a55b511658348e15652c7427
tdelpier <tdelpier@gmail.com>
2026-01-12 12:24:53 -0500

    new flextable format

--- R/fa_setup_flextable_set.R
+++ R/fa_setup_flextable_set.R
@@ -23,25 +23,45 @@ fa_setup_flextable_set <- function() {
 #'
 #'
 #' @export
-flextable_custom_theme <- function(x){
+flextable_custom_theme <- function(flex_table, title){
   
-  #all
-  x <- fontsize(x, size = 10, part = "all")
-  
-  # header
-  x <- align(x, align = "center", part = "header")
-  x <- bold(x, bold = TRUE, part = "header")
-  x <- line_spacing(x, space = 0.5, part = "body")
-  
-  
-  
-  # body
-  
-  # x  <- font(x, fontname = "Arial", part = "all")
-  
-  x %>% 
-    autofit() %>%   # prints x at the end
-    htmltools_value(ft.shadow = FALSE)
+  {{ flex_table }} %>% 
+    flextable::font(fontname = "Helvetica", part = "all") %>% 
+    
+    set_caption(caption = as_paragraph(as_chunk({{ title }}, propos = fp_text_default(font.family = "Helvetica"))) ,
+                fp_p = officer::fp_par(text.align = "left"),
+                align_with_table = FALSE) %>% 
+    
+    
+    fontsize(size = 10, part = "all") %>% 
+    
+    align(align = "center", part = "header") %>% 
+    bold(bold = TRUE, part = "header") %>% 
+    # hline_top(border = fp_border_default(width = 0), part = "header") %>% 
+    line_spacing(space = .5, part = "body") %>% 
+    autofit()  
   
 } 
 
+
+
+# flextable_custom_theme <- function(x){
+#   
+#   #all
+#   x <- fontsize(x, size = 10, part = "all")
+#   
+#   # header
+#   x <- align(x, align = "left", part = "header")
+#   x <- bold(x, bold = TRUE, part = "header")
+#   x <- hline_top(x, border = fp_border_default(width = 0), part = "header")
+#   x <- line_spacing(x, space = 0.5, part = "body")
+#   
+#   # body
+#   
+#   # x  <- font(x, fontname = "Arial", part = "all")
+#   
+#   x %>%
+#     autofit() %>%   # prints x at the end
+#     htmltools_value(ft.shadow = FALSE)
+#   
+# } 
