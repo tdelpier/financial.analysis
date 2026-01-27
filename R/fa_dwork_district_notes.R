803876e1db7e397035297370b45cc63e8d45d06c
tdelpier <tdelpier@gmail.com>
2026-01-27 08:42:23 -0500

    added notes on non-GF GF funds

--- R/fa_dwork_district_notes.R
+++ R/fa_dwork_district_notes.R
@@ -22,8 +22,31 @@ fa_helper_add_district_notes <- function(df, dnum, note) {
 fa_dwork_district_notes <- function(df) {
   
   df %>%
-    fa_helper_add_district_notes(82090, "The district maintains a seperate Special Education fund. ") %>% 
-  
+   
+  # districts with "Funded Project" or "Federal, State, and Local Grant Funds" aggregated into GF by FID
+    
+    # Dearborn
+    fa_helper_add_district_notes(82030, 'The district maintains a "Funded Projects" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund. Budget data for the "Funded Projects" fund was not in FY 2015-2017, 2019, or 2020.') %>% 
+    
+    # Taylor
+    fa_helper_add_district_notes(82150, 'The district maintains a "Federal, State, and Local Grants" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund.' ) %>% 
+    
+    # Wayne-Westland
+    fa_helper_add_district_notes(82160, 'The district maintains a "Federal, State, and Local Grants" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund.' ) %>% 
+    
+    # Lincoln Park
+    fa_helper_add_district_notes(82090, 'The district maintains a "Funded Projects" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund.') %>% 
+    
+    # Livonia
+    fa_helper_add_district_notes(82095, 'The district maintains a "Funded Projects" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund.') %>% 
+    
+    # Plymouth Canton
+    fa_helper_add_district_notes(82100, 'The district maintains a "Funded Projects" fund that is not recognized by state reporting in the FID. This funded is aggregated into the General Fund. Budget data for the "Funded Projects" fund was not in FY 2015-2019.') %>% 
+    
+    
+  # Other Notes
+    
+    # Baldwin
     fa_helper_add_district_notes(43040, "Baldwin changed their fund structure in 2019 resulting in a large reduction in both General Fund revenue and expenditure.") 
     
    
