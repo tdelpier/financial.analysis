09e0181d8e7379b95bc3dc51fb2dc68c07431eb1
tdelpier <tdelpier@gmail.com>
2026-01-27 08:56:07 -0500

    adding pages and description to my main testing script

--- fa_goal_main.R
+++ fa_goal_main.R
@@ -6,50 +6,50 @@ library(financial.analysis)
 library(testthat)
 
 # setup 
-fiscal.year <- 2025
+fiscal.year <- 2026
 fa_setup()
 
-
-
-# construct the data from scratch
+# construct the data from scratch and store as "raw" in data directory
 fa_construct_data()
+
+# imports the "raw" data and perfomrs dwork on it. 
+  # Then dumps that data into "prepped" in the data directory
 fa_import_data("raw") %>% fa_dwork()
 
-# load the data
+
+# load the most recent "prepped" data
 FA_Data <- fa_import_data("prepped")
+
+
+# loads the district geography data
 dist_geo <- fa_import_geo() %>% rename(geometry = geom)
 
-fund_names <- 
-  TannersTools::FID_names_fund %>% 
-  mutate(var.name = paste0("fid.b.fb.", fund))
-recode_vec <- setNames(fund_names$var.name, fund_names$fund.name)
 
 
 # Choosing District 
  tt_dnum_from_dname("kent")
  tt_dnum_from_dname("benzie")
- tt_dnum_from_dname("utica")
+ tt_dnum_from_dname("wayne")
  
  
 dnum_x <- tt_dnum_random(1)
-# dnum_x <- 15010 # beaver island
+dnum_x <- 15010 # beaver island
 # dnum_x <- 18010 # Clare
 # dnum_x <- 82055 # Grosse Pointe 
 # dnum_x <- 43040 # baldwin
 dnum_x <- 52170 # Marquette
 # dnum_x <-  33020 # Lansing
-# dnum_x <- 18020
 dnum_x <- 76210 #sandusky
 dnum_x <- 82090 # Lincoln Park
-dnum_x <- 82160
-dnum_x <- 54025
 dnum_x <- 82340 # huron problem
 dnum_x <- 39065 # gull lake
-dnum_x <- 25180
-dnum_x <- 10015
 dnum_x <- 41050 # Caledonia        
 dnum_x <- 50210 # Utica
-
+dnum_x <- 70020 # holand
+dnum_x <- 2070 # munising
+dnum_x <- 25010 # flint
+dnum_x <- 50070 # Clintondale
+dnum_x <- 82160 # wayne westland
 
 FA_Data_District <-  
   FA_Data %>%
@@ -59,29 +59,30 @@ FA_Data_District <-
   ungroup()
 
 
+
+
 FA_Data_District %>% 
   fa_helper_render_district_fa(dnum_x,
-                               type = "paged", # "web", "paged", or "slides"
+                               type = "web", # "web", "paged", or "slides"
                                page.details = FALSE,
-                               page.revest = FALSE, 
-                               page.budcomp = FALSE,
-                               page.stim = FALSE,
+                               page.revest = TRUE, 
+                               page.budcomp = TRUE,
                                page.sam = FALSE,
                                page.found = FALSE,
                                page.revexp = FALSE,
-                               page.surplus = FALSE,
-                               page.gfb = TRUE,
-                               page.transfers = TRUE,
+                               page.gfb = FALSE,
+                               page.transfers = FALSE,
+                               page.millages = FALSE,
+                               page.stutransfers = FALSE,
+                               page.econdisad = FALSE,
                                page.methods = FALSE,
                                
                                # not using
-                               page.millages = FALSE,
-                               page.cac = FALSE
+                               page.stim = FALSE,
+                               page.cac = FALSE, 
+                               page.surplus = FALSE
+                               
                                )
-
-
-
-
 
 
 
