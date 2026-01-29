c8e7dd17222b2171b017ae74e7e9735437472267
tdelpier <tdelpier@gmail.com>
2026-01-29 12:24:14 -0500

    I'm about 90% sure I have the estimate right. Now I have to hook it into the rest of the system.

--- R/fa_dwork_workingon_est.R
+++ R/fa_dwork_workingon_est.R
@@ -1,5 +1,12 @@
 
-fun_est_temp <- function(df){
+
+
+#' Dwork estimations
+#'
+#' 
+#'
+#' @export
+dwork_est_2 <- function(df){
   
   {{ df }} %>%
     
@@ -23,25 +30,13 @@ fun_est_temp <- function(df){
     create_tabrev_var_from_icd(var_name = "tab.rev.state.31aa" ,           c("174")) %>% 
     create_tabrev_var_from_icd(var_name = "tab.rev.state.29",              c("076")) %>% 
     create_tabrev_var_from_icd(var_name = "tab.rev.state.22l"  ,           c("106")) %>% 
-    create_tabrev_var_from_icd(var_name = "tab.rev.state.cte",             c("470", "088")) %>% 
+    create_tabrev_var_from_icd(var_name = "tab.rev.state.cte",             c("470", "088", "490")) %>% 
     create_tabrev_var_from_icd(var_name = "tab.rev.state.edcomp",          c("cy.adj.1.858")) %>%  
     
     create_tabrev_var_from_icd(var_name = "tab.rev.state.mpsers.uaal" ,    c("573", "583", "598")) %>% 
     create_tabrev_var_from_icd(var_name = "tab.rev.state.mpsers.netother", c("597", "cy.adj.1.567")) %>% 
-    
     create_tabrev_var_from_icd(var_name = "tab.rev.state.mspers.other" ,   c("079", "082", "572", "588", "587", "cy.adj.1.079")) %>% 
- 
-##########################################################################################################################
-##################################### this is the chunk I'm working on 
-##########################################################################################################################
     
-  create_tabrev_var_from_icd(rev.state.other.1 ) # things to keep unchanged and aggregated
-  muate(rev.state.other.2 = things to change, 
-        tab.rev.state.other = rev.state.other.1  + rev.state.other.2)
-  
-  
-  
-  # Manual 
     mutate(
       tab.rev.state.mspers.other = 
         case_when(FY == 2026 ~
@@ -49,55 +44,29 @@ fun_est_temp <- function(df){
                      +(lag(cy.a.8.082) * 1.13) #147e
                      + cy.a.1.572 #147a(1)
                      + cy.a.1.588 + #147a(3)
-                     + cy.adj.1.079  #147a(2) additional $50 million allocation adjustment for FY25
+                       + cy.adj.1.079  #147a(2) additional $50 million allocation adjustment for FY25
                      + cy.a.1.587),
                   .default = tab.rev.state.mspers.other)) %>% 
-    
-    create_manual_add_sub(FY = 2026, suffix = "104h", manual_add = lag(cy.a.8.096), manual_sub = 0) %>% 
-    create_manual_add_sub(FY = 2026, suffix = "35m", manual_add = lag(cy.a.8.865) * 0.8, manual_sub = cy.a.1.865) %>% 
-    # create_manual_add_sub(FY = 2026, suffix = "147a2", manual_add = ((lag(cy.a.8.079) * 0.92)) , manual_sub = 0) %>%
-    # create_manual_add_sub(FY = 2026, suffix = "147e", manual_add = (lag(cy.a.8.082) * 1.13), manual_sub = 0) %>%
-    # create_manual_add_sub(FY = 2026, suffix = "147a2add", manual_add = cy.adj.1.079, manual_sub = 0) %>%
+ 
 
-      
-    create_tabrev_var_from_icd(var_name = "uncounted.rev.state" ,   c("235", "335", "354")) %>% 
-    mutate(listed.counted.rev.state = rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE),
-           manual.sub = rowSums(across(any_of(starts_with("manual.sub."))), na.rm = TRUE),
-           manual.add = rowSums(across(any_of(starts_with("manual.add."))), na.rm = TRUE),
-           manual.change = manual.add - manual.sub, 
-           safsr.fidr.error = fid.r.state - listed.counted.rev.state, 
-           
-           tab.rev.state = ifelse(FY != fiscal.year, fid.r.state, NA),
-           
-           tab.rev.state.other = ifelse(FY != fiscal.year,  tab.rev.state - listed.counted.rev.state, NA),
-           tab.rev.state.other = ifelse(FY == 2026, 
-                                        manual.add.35m + manual.add.104h + cy.a.1.848  + lag(safsr.fidr.error),
-                                        tab.rev.state.other),
-           
-           
-           # tab.rev.state.other = ifelse(FY == fiscal.year,
-           #                              cy.a.1.000 
-           #                              - listed.counted.rev.state 
-           #                              + manual.change 
-           #                              + lag(safsr.fidr.error)
-           #                              # - uncounted.rev.state
-           #                              - cy.a.1.9999 - cy.a.1.8888,
-           #                              tab.rev.state - listed.counted.rev.state), 
-           # need to subtract out this - cy.a.1.9999 - cy.a.1.8888,
-           
-           tab.rev.state = ifelse(FY == fiscal.year, 
-                                  rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE),
-                                  tab.rev.state)) %>% 
-
+  create_tabrev_var_from_icd(var_name = "in.jan", c("037", "046", "064", "171", "175", "228", "251", "666", "848", "1023")) %>% # things to keep unchanged and aggregated
+  create_tabrev_var_from_icd(var_name = "onetime.safsr", c("1023") ) %>% 
+  create_tabrev_var_from_icd(var_name = "non.gf.safsr.rev" ,   c("235", "335", "354")) %>% 
     
-  #   + lag(fid.r.state.to.cy.a.8.000.error)
-  # 
-  # 
-  # fid.r.state.to.cy.a.8.000.error = fid.r.state - (cy.a.8.000 - est.8.rev.state.meals),
-##########################################################################################################################
-##########################################################################################################################
-##########################################################################################################################
-##########################################################################################################################
+  mutate(manual = 
+          lag(cy.a.8.096) +  # 104h
+          (lag(cy.a.8.865) * 0.8) + #35m
+           lag(cy.a.8.260),  #99h
+         
+        rev.state.safsr.fid.error = (fid.r.state - onetime.safsr) - (cy.a.8.000 - non.gf.safsr.rev), 
+        listed.counted.rev.state = rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE),
+        tab.rev.state.other = ifelse(FY != fiscal.year,  fid.r.state - listed.counted.rev.state, NA),
+        tab.rev.state.other = ifelse(FY == fiscal.year, 
+                                     in.jan + manual + lag(rev.state.safsr.fid.error), 
+                                     tab.rev.state.other),
+        tab.rev.state = rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE)
+        ) %>%  
+
 
   # Federal
     
@@ -144,30 +113,4 @@ fun_est_temp <- function(df){
 
 
 
-
-# mutate(
-#   unlisted.counted.rev.state.104h = ifelse(FY == 2026, lag(cy.a.8.096), 0),
-#   unlisted.counted.rev.state.35m = ifelse(FY == 2026, lag(cy.a.8.865) * 0.8, 0),
-#   
-#   unidentified.counted.rev.state = cy.a.1.000 - uncounted.rev.state + unlisted.counted.rev.state + listed.counted.rev.state
-# ) %>% 
-# create_tabrev_var_from_icd(var_name = "unlisted.counted.other", c()) %>% 
-# 
-# create_tabrev_var_from_icd(var_name = "uncounted.rev.state" ,   c("235", "335", "354")) %>% 
-# 
-# mutate(
-#   unlisted.counted.rev.state = rowSums(across(any_of(starts_with("unlisted.counted."))),
-#   listed.counted.rev.state = rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE),
-# 
-#   
-#   
-#   tab.rev.state.other = ifelse(FY == fiscal.year, 
-#                                xx1, # what to put here is probably one of my biggest challenges 
-#                                
-#                                fid.r.state - listed.counted.rev.state),  
-#   
-#   tab.rev.state = listed.counted.rev.state + unlisted.counted.rev.state, 
-#   # tab.rev.state = rowSums(across(any_of(starts_with("tab.rev.state."))), na.rm = TRUE)
-#   
-# ) %>% 
 
